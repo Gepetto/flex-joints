c7841c818a870edcb932e047c773fe6062c516aa
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-10-07 19:35:04 +0200

    Nix: BUILD_STANDALONE_PYTHON_INTERFACE

--- flake.nix
+++ flake.nix
@@ -24,8 +24,8 @@
         }:
         {
           packages = {
-            default = self'.packages.flex-joints;
-            flex-joints = pkgs.python3Packages.flex-joints.overrideAttrs {
+            default = self'.packages.py-flex-joints;
+            flex-joints = pkgs.flex-joints.overrideAttrs {
               src = lib.fileset.toSource {
                 root = ./.;
                 fileset = lib.fileset.unions [
@@ -38,6 +38,23 @@
                 ];
               };
             };
+            py-flex-joints = pkgs.python3Packages.toPythonModule (
+              self'.packages.flex-joints.overrideAttrs (super: {
+                pname = "py-${super.pname}";
+                cmakeFlags = super.cmakeFlags ++ [
+                  (lib.cmakeBool "BUILD_PYTHON_INTERFACE" true)
+                  (lib.cmakeBool "BUILD_STANDALONE_PYTHON_INTERFACE" true)
+                ];
+                propagatedBuildInputs = (super.propagatedBuildInputs or [ ]) ++ [
+                  self'.packages.flex-joints
+                  pkgs.python3Packages.boost
+                  pkgs.python3Packages.eigenpy
+                ];
+                nativeCheckInputs = [
+                  pkgs.python3Packages.pythonImportsCheckHook
+                ];
+              })
+            );
           };
         };
     };
