d5d868e3103150dbc8ba24524c503c77ac8fe93d
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-03-27 19:49:43 +0100

    nix: switch to flakoboros

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     gepetto.url = "github:gepetto/nix";
+    flakoboros.follows = "gepetto/flakoboros";
     gazebros2nix.follows = "gepetto/gazebros2nix";
     flake-parts.follows = "gepetto/flake-parts";
     nixpkgs.follows = "gepetto/nixpkgs";
@@ -13,50 +14,45 @@
 
   outputs =
     inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = import inputs.systems;
-      imports = [ inputs.gepetto.flakeModule ];
-      perSystem =
-        {
-          lib,
-          pkgs,
-          self',
-          ...
-        }:
-        {
-          packages = {
-            default = self'.packages.py-flex-joints;
-            flex-joints = pkgs.flex-joints.overrideAttrs {
-              src = lib.fileset.toSource {
-                root = ./.;
-                fileset = lib.fileset.unions [
-                  ./include
-                  ./src
-                  ./python
-                  ./tests
-                  ./CMakeLists.txt
-                  ./package.xml
-                ];
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+      { lib, ... }:
+      {
+        systems = import inputs.systems;
+        imports = [
+          inputs.gepetto.flakeModule
+          {
+            flakoboros = {
+              overrideAttrs.flex-joints = _: _: {
+                src = lib.fileset.toSource {
+                  root = ./.;
+                  fileset = lib.fileset.unions [
+                    ./include
+                    ./src
+                    ./python
+                    ./tests
+                    ./CMakeLists.txt
+                    ./package.xml
+                  ];
+                };
               };
+              pyOverrideAttrs.flex-joints =
+                _: pyFinal:
+                (super: {
+                  cmakeFlags = super.cmakeFlags ++ [
+                    (lib.cmakeBool "BUILD_PYTHON_INTERFACE" true)
+                    (lib.cmakeBool "BUILD_STANDALONE_PYTHON_INTERFACE" true)
+                  ];
+                  propagatedBuildInputs = (super.propagatedBuildInputs or [ ]) ++ [
+                    pyFinal.boost
+                    pyFinal.eigenpy
+                  ];
+                  nativeCheckInputs = [
+                    pyFinal.pythonImportsCheckHook
+                  ];
+                });
             };
-            py-flex-joints = pkgs.python3Packages.toPythonModule (
-              self'.packages.flex-joints.overrideAttrs (super: {
-                pname = "py-${super.pname}";
-                cmakeFlags = super.cmakeFlags ++ [
-                  (lib.cmakeBool "BUILD_PYTHON_INTERFACE" true)
-                  (lib.cmakeBool "BUILD_STANDALONE_PYTHON_INTERFACE" true)
-                ];
-                propagatedBuildInputs = (super.propagatedBuildInputs or [ ]) ++ [
-                  self'.packages.flex-joints
-                  pkgs.python3Packages.boost
-                  pkgs.python3Packages.eigenpy
-                ];
-                nativeCheckInputs = [
-                  pkgs.python3Packages.pythonImportsCheckHook
-                ];
-              })
-            );
-          };
-        };
-    };
+          }
+        ];
+      }
+    );
 }
